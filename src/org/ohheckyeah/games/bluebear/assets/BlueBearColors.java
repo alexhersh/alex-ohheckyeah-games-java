947c5313159e0e9b6664f81df7ca6d3a9e6f26ce
cacheflowe <cacheflowe@cacheflowe.com>
2014-04-30 00:16:39 -0600

    Set background to white

--- src/org/ohheckyeah/games/bluebear/assets/BlueBearColors.java
+++ src/org/ohheckyeah/games/bluebear/assets/BlueBearColors.java
@@ -6,7 +6,7 @@ import com.haxademic.core.draw.color.ColorUtil;
 public class BlueBearColors
 extends ColorGroup {
 	
-	public final static int STAGE_BG = ColorUtil.colorFromHex("#E7E867");
+	public final static int STAGE_BG = ColorUtil.colorFromHex("#ffffff");
 	public final static int MAIN_TEXT_COLOR = ColorUtil.colorFromHex("#000000");
 	public final static int CREDITS_BG = ColorUtil.colorFromHex("#94C65B");
 	public final static int INTRO_SCREENS_BG = ColorUtil.colorFromHex("#ffffff");
