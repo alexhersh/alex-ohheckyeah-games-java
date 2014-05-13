b2d86942b0f8eeaffd1d2b2f0d07522310d735f8
cacheflowe <cacheflowe@cacheflowe.com>
2014-05-13 00:54:38 -0600

    Uupdate color definitions

--- src/org/ohheckyeah/games/bluebear/assets/BlueBearColors.java
+++ src/org/ohheckyeah/games/bluebear/assets/BlueBearColors.java
@@ -8,10 +8,12 @@ extends ColorGroup {
 	
 	public final static int STAGE_BG = ColorUtil.colorFromHex("#ffffff");
 	public final static int MAIN_TEXT_COLOR = ColorUtil.colorFromHex("#000000");
-	public final static int CREDITS_BG = ColorUtil.colorFromHex("#94C65B");
+	public final static int CREDITS_BG = ColorUtil.colorFromHex("#f6921e");
+	public final static int CREDITS_DOTS = ColorUtil.colorFromHex("#f9a137");
 	public final static int INTRO_SCREENS_BG = ColorUtil.colorFromHex("#ffffff");
-	public final static int TITLE_SCREEN_BG = ColorUtil.colorFromHex("#94C65B");
-	public final static int TITLE_SCREEN_BORDER = ColorUtil.colorFromHex("#393427");
+	public final static int TITLE_SCREEN_BG = ColorUtil.colorFromHex("#fffbd4");
+	public final static int DETECT_SCREEN_TOP = ColorUtil.colorFromHex("#f07db1");
+	public final static int DETECT_SCREEN_BOTTOM = ColorUtil.colorFromHex("#f072ab");
 	
 	public BlueBearColors( int set ) {
 		super(set);
