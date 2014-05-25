87811e82a6be2e0b191f167fe09241d9c28e368d
cacheflowe <cacheflowe@cacheflowe.com>
2014-05-25 17:58:31 -0600

    Refactor base remote control app

--- src/org/ohheckyeah/games/bluebear/BlueBearRemoteControl.java
+++ src/org/ohheckyeah/games/bluebear/BlueBearRemoteControl.java
@@ -1,6 +1,6 @@
-package org.ohheckyeah.games.catchy;
+package org.ohheckyeah.games.bluebear;
 
-import org.ohheckyeah.games.catchy.assets.CatchyColors;
+import org.ohheckyeah.games.bluebear.assets.BlueBearColors;
 import org.ohheckyeah.shared.OHYBaseRemoteControl;
 
 import processing.core.PApplet;
@@ -10,7 +10,7 @@ import com.haxademic.core.draw.util.DrawUtil;
 import com.haxademic.core.system.FileUtil;
 
 @SuppressWarnings("serial")
-public class CatchyRemoteControl
+public class BlueBearRemoteControl
 extends OHYBaseRemoteControl  
 {
 
@@ -18,12 +18,12 @@ extends OHYBaseRemoteControl
 	
 	public static void main(String args[]) {
 		_isFullScreen = true;
-		PApplet.main(new String[] { "--hide-stop", "--bgcolor=000000", "org.ohheckyeah.games.catchy.CatchyRemoteControl" });
+		PApplet.main(new String[] { "--hide-stop", "--bgcolor=000000", "org.ohheckyeah.games.bluebear.BlueBearRemoteControl" });
 	}
 
 	public void setup() {
-		super.setup( "catchy.properties" );
-		_logo = p.loadShape( FileUtil.getHaxademicDataPath() + "games/catchy/svg/shell/catchy-logo.svg" );
+		super.setup( "bluebear.properties" );
+		_logo = p.loadShape( FileUtil.getHaxademicDataPath() + "games/bluebear/svg/shell/big-blue-logo.svg" );
 	}
 	
 	protected void overridePropsFile() {
@@ -34,7 +34,7 @@ extends OHYBaseRemoteControl
 		super.drawApp();
 		
 		// draw branded game screen 
-		p.background( CatchyColors.TITLE_SCREEN_BG );
+		p.background( BlueBearColors.TITLE_SCREEN_BG );
 		DrawUtil.setDrawCenter(p);
 		float drawRatio = ( p.width / _logo.width ) * 0.7f;
 		p.shape(_logo, p.width * 0.5f, p.height * 0.5f, _logo.width * drawRatio, _logo.height * drawRatio );
---
+++ src/org/ohheckyeah/shared/OHYBaseRemoteControl.java
@@ -0,0 +1,80 @@
+package org.ohheckyeah.shared;
+
+import hypermedia.net.UDP;
+
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.hardware.kinect.KinectRegion;
+import com.haxademic.core.hardware.kinect.KinectRegionGrid;
+import com.haxademic.core.system.FileUtil;
+
+public class OHYBaseRemoteControl
+extends PAppletHax{
+
+	// input
+	public static float KINECT_MIN_DIST = 1500;
+	public static float KINECT_MAX_DIST = 2000;
+	public static int KINECT_TOP = 0;
+	public static int KINECT_BOTTOM = 480;
+	
+	public static int NUM_PLAYERS = 2;
+	protected KinectRegionGrid _kinectGrid;
+	
+	protected UDP _udp;
+	protected boolean _debugging = false;
+	protected String _receiverIp = "";
+	protected int _receiverPort = 0;
+
+	public void setup( String propertiesFile ) {
+		_customPropsFile = FileUtil.getHaxademicDataPath() + "properties/" + propertiesFile;
+		super.setup();
+		initControls();
+		initUDP();
+	}
+	
+	protected void initControls() {
+		setKinectProperties();
+		_kinectGrid = new KinectRegionGrid(p, NUM_PLAYERS, 1, (int)KINECT_MIN_DIST, (int)KINECT_MAX_DIST, 250, (int)KINECT_TOP, (int)KINECT_BOTTOM);
+	}
+	
+	protected void setKinectProperties() {
+		// default pixel rows are the center 200 kinect data rows
+		KINECT_MIN_DIST = _appConfig.getInt( "kinect_min_mm", 1500 );
+		KINECT_MAX_DIST = _appConfig.getInt( "kinect_max_mm", 2000 );
+		KINECT_TOP = _appConfig.getInt( "kinect_top_pixel", 240 );
+		KINECT_BOTTOM = _appConfig.getInt( "kinect_bottom_pixel", 400 );
+		NUM_PLAYERS = _appConfig.getInt( "num_players", 2 );
+		
+		_debugging = _appConfig.getBoolean( "kinect_remote_debug", false );
+		_receiverIp = _appConfig.getString( "kinect_remote_receiver_ip", "" );
+		_receiverPort = _appConfig.getInt( "kinect_remote_receiver_port", 0 );
+	}
+	
+	protected void initUDP() {
+		_udp = new UDP( this, 9999 );	// TODO: fix 9999????
+		_udp.log( _debugging );
+		_udp.listen( true );	
+	}
+	
+	public void drawApp() {
+		_kinectGrid.update();
+		sendKinectGrid( buildKinectString() );
+	}
+	
+	protected String buildKinectString() {
+		String kinectOutput = "";
+		KinectRegion kinectRegion = null;
+		for( int i=0; i < NUM_PLAYERS; i++ ) {
+			kinectRegion = _kinectGrid.getRegion(i);
+			if( i > 0 ) kinectOutput += "|"; 
+			kinectOutput += kinectRegion.controlX() + ":" + kinectRegion.controlZ() + ":" + kinectRegion.pixelCount();
+		}
+		return kinectOutput;
+	}
+	
+	protected void sendKinectGrid( String kinectData ) {
+		if( _debugging == true ) P.println("kinectData = "+kinectData);
+		_udp.send( kinectData, _receiverIp, _receiverPort );
+	}
+
+}
