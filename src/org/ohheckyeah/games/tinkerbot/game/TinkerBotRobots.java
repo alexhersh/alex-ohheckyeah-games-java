cb8305be14f16a592dc73f0554ca78aec5bc95dc
cacheflowe <cacheflowe@cacheflowe.com>
2014-05-30 18:38:04 -0600

    More robot drawing. Top & bottom ball parts are in

--- src/org/ohheckyeah/games/tinkerbot/game/TinkerBotRobots.java
+++ src/org/ohheckyeah/games/tinkerbot/game/TinkerBotRobots.java
@@ -19,7 +19,14 @@ public class TinkerBotRobots {
 	protected float _robotX;
 	protected EasingFloat _robotXOffset;
 	protected float _robotXOffsetHidden;
+	
+	protected float _ballWidth;
+	protected float _ballHeight;
 
+	protected EasingFloat _ballYOffset;
+	protected float _ballYOffsetHidden;
+	protected float _ballYOffsetShowing;
+	
 	public TinkerBotRobots() {
 		p = (TinkerBot)P.p;
 		pg = p.pg;
@@ -29,35 +36,79 @@ public class TinkerBotRobots {
 		_robotX = pg.width * 0.1f;
 		_robotXOffsetHidden = pg.width * -0.3f;
 		_robotXOffset = new EasingFloat( _robotXOffsetHidden, 6 );
+		
+		_ballWidth = p.svgWidth( p.gameGraphics.robotBallTop );
+		_ballHeight = p.svgHeight( p.gameGraphics.robotBallTop );
+		
+		_ballYOffsetHidden = _ballHeight * 1f;
+		_ballYOffsetShowing = _ballHeight * 0.1f;
+		_ballYOffset = new EasingFloat( _ballYOffsetHidden, 6 );
+	}
+	
+	public void show() {
+		_robotXOffset.setTarget(0);
+		_ballYOffset.setTarget(_ballYOffsetShowing);
+	}
+
+	public void hide() {
+		_robotXOffset.setTarget(_robotXOffsetHidden);
+		_ballYOffset.setTarget(_ballYOffsetHidden);
 	}
 	
 	public void update() {
 		// easing
 		_robotXOffset.update();
+		_ballYOffset.update();
 		
 		// draw bars
 		DrawUtil.setDrawCenter(pg);
 		
+		// left robot ================================================================
+		float leftX = _robotX + _robotXOffset.value();
+		
 		// draw left bar
 		pg.pushMatrix();
-		pg.translate( _robotX + _robotXOffset.value(), pg.height * 0.5f );
+		pg.translate( leftX, pg.height * 0.5f );
 		pg.shape(_barSvg, 0, 0, _barSvgWidth, pg.height );
 		pg.popMatrix();
 		
+		// draw top robot ball
+		pg.pushMatrix();
+		pg.translate( leftX, 0 - _ballYOffset.value() );
+		pg.shape(p.gameGraphics.robotBallTop, 0, 0, _ballWidth, _ballHeight );
+		pg.popMatrix();
+		
+		// draw bottom robot ball
+		pg.pushMatrix();
+		pg.translate( leftX, pg.height + _ballYOffset.value() );
+		pg.shape(p.gameGraphics.robotBallBottom, 0, 0, _ballWidth, _ballHeight );
+		pg.popMatrix();
+		
+		
+		// right robot ================================================================
+		float rightX = (pg.width - _robotX) - _robotXOffset.value();
+		
 		// draw right bar
 		pg.pushMatrix();
-		pg.translate( (pg.width - _robotX) - _robotXOffset.value(), pg.height * 0.5f );
+		pg.translate( rightX, pg.height * 0.5f );
 		pg.rotate( P.PI );
 		pg.shape(_barSvg, 0, 0, _barSvgWidth, pg.height );
 		pg.popMatrix();
-	}
-	
-	public void show() {
-		_robotXOffset.setTarget(0);
-	}
 
-	public void hide() {
-		_robotXOffset.setTarget(_robotXOffsetHidden);
+		// draw top robot ball
+		pg.pushMatrix();
+		pg.translate( rightX, 0 - _ballYOffset.value() );
+		pg.rotate( P.PI );
+		pg.shape(p.gameGraphics.robotBallBottom, 0, 0, _ballWidth, _ballHeight );
+		pg.popMatrix();
+		
+		// draw bottom robot ball
+		pg.pushMatrix();
+		pg.translate( rightX, pg.height + _ballYOffset.value() );
+		pg.rotate( P.PI );
+		pg.shape(p.gameGraphics.robotBallTop, 0, 0, _ballWidth, _ballHeight );
+		pg.popMatrix();
+
 	}
 	
 }
